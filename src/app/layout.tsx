f767592ed3d113a6aff241837bf457f43336de02
Roberto Rodríguez Rodríguez <roberto.rodriguez@n26.com>
2024-09-18 12:23:15 +0200

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,41 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: "Copa Turbo",
+    title: "Copa Turbo",
 };
 
 export default function RootLayout({
-									   children,
-								   }: Readonly<{
-	children: React.ReactNode;
+    children,
+}: Readonly<{
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-		<body
-			className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-		>
-		{children}
-		</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <head>
+                <script
+                    defer
+                    data-domain="copaturbo.es"
+                    src="https://plausible-analytics-ce-production-37be.up.railway.app/js/script.js"
+                ></script>
+            </head>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
